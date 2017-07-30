ed5d9dadcee6948e9be4deb8fd7c01361b51bd70
shinobispb <shinobispb@gmail.com>
2017-07-30 22:44:33 +0400

    adding not null restrict

--- docker/queue_db/queue_db.sql
+++ docker/queue_db/queue_db.sql
@@ -1,17 +1,17 @@
 CREATE TABLE IF NOT EXISTS token (
 	token_id SERIAL PRIMARY KEY,
-	access_token varchar(128)
+	access_token varchar(128) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS event (
 	event_id SERIAL PRIMARY KEY,
-	event_code varchar(255)
+	event_code varchar(255) NOT NULL
 );
 
 
 CREATE TABLE IF NOT EXISTS stream (
 	stream_id SERIAL PRIMARY KEY,
-	stream_type varchar(64)
+	stream_type varchar(64) NOT NULL
 );
 
 
@@ -21,8 +21,8 @@ CREATE TABLE IF NOT EXISTS msg (
 	token_id SERIAL references token (token_id),
 	event_id SERIAL references event (event_id),
 	stream_id SERIAL references stream(stream_id),
-	to_ varchar(128),
-	data json
+	to_ varchar(128) NOT NULL,
+	data json NOT NULL
 );
 
 
@@ -31,5 +31,3 @@ CREATE TABLE IF NOT EXISTS msg (
 insert into stream (stream_type) VALUES('email');
 insert into stream (stream_type) VALUES('sms');
 insert into stream (stream_type) VALUES('push');
-
-
